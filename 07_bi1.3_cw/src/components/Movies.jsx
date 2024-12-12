4e4193026a6bdc252d937abaff1094aa80380b3a
AnishKarchuli <aksk099@hotmail.com>
2024-12-12 23:00:20 +0530

    Finished till BI1.3_HW2

--- 07_bi1.3_cw/src/components/Movies.jsx
+++ 07_bi1.3_cw/src/components/Movies.jsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
 import useFetch from "../useFetch";
 
 const Movies = () => {
+
+  const [successMessage, setSuccessMessage] = useState("");
+
   const { data, loading, error } = useFetch("http://localhost:3000/movies");
+
+  const handleDelete = async (movieId) => {
+    try{
+      const response = await fetch(`http://localhost:3000/movies/${movieId}`,
+        {
+          method: "DELETE"
+        }
+      )
+
+      if(!response.ok){
+        throw 'Failed to delete movie.'
+      }
+
+      const data = await response.json();
+      if(data){
+        setSuccessMessage('Movie deleted successfully.');
+        window.location.reload();
+      }
+    }catch(error){
+      console.log(error)
+    }
+  }
 
   return (
     <div>
       <ul>
         {data?.map((movie) => (
-          <li key={movie._id}>{movie.title}</li>
+          <li key={movie._id}>
+            {movie.title} <button onClick={() => handleDelete(movie._id)}>Delete</button>
+          </li>
         ))}
       </ul>
+      <p>{successMessage}</p>
     </div>
   );
 };
--- 08_bi1.3_hw1/src/components/Books.jsx
+++ 08_bi1.3_hw1/src/components/Books.jsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
 import useFetch from "../useFetch";
 
 const Books = () => {
-    const { data, loading, error } = useFetch("http://localhost:3000/books")
+  const [successMessage, setSuccessMessage] = useState("");
 
-    return (
-        <div>
-            <h2>All Books</h2>
-            <ul>
-                {data?.map((book) => (
-                    <li key={book._id}>{book.title}</li>
-                ))}
-            </ul>
-        </div>
-    )
+  const { data, loading, error } = useFetch("http://localhost:3000/books");
+
+  const handleDelete = async (bookId) => {
+    try {
+      const response = await fetch(`http://localhost:3000/books/${bookId}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw "Failed to delete a book.";
+      }
+
+      const data = await response.json();
+      if (data) {
+        setSuccessMessage("Book deleted successfully.");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>All Books</h2>
+      <ul>
+        {data?.map((book) => (
+          <li key={book._id}>
+            {book.title}{" "}
+            <button onClick={() => handleDelete(book._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Books;
